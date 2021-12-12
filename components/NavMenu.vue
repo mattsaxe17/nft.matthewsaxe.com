6946c94f67f20f3570d3a9c75eb27c5baaa3bc65
Matthew Saxe <mattsaxe17@gmail.com>
2021-12-12 10:26:34 -0500

    Reconfigure nuxt application

---
+++ components/NavMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="nav-bar">
+    <nav class="d-none d-md-flex">
+      <nuxt-link to="#about" class="menu-link"><span class="prefix">01.</span>About</nuxt-link>
+      <nuxt-link to="#projects" class="menu-link"><span class="prefix">02.</span>Projects</nuxt-link>
+      <nuxt-link to="#contact" class="menu-link"><span class="prefix">03.</span>Contact</nuxt-link>
+      <!-- <nuxt-link to="/blog">Blog</nuxt-link> -->
+    </nav>
+
+    <img id="mobile-menu" class="d-inline d-md-none" src="/menu.svg" alt="menu icon" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#nav-bar {
+  height: 50%;
+
+  nav {
+    display: flex;
+    gap: 1em;
+
+    .menu-link {
+      color: white;
+      text-decoration: none;
+
+      .prefix {
+        color: $primary-color;
+        font-weight: bold;
+
+        &::after {
+          content: " "
+        }
+      }
+    }
+  }
+
+  #mobile-menu {
+    height: 100%;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <div id="app-bar" ref="appBar">
+      <nuxt-link to="/" class="menu-link logo">
+        <img src="/logo.svg" alt="logo" />
+      </nuxt-link>
+
+      <v-spacer />
+
+      <nav-menu />
+    </div>
+  </v-app>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      scrollPos: 0,
+    }
+  },
+  mounted() {
+    window.onscroll = () => {
+      const currentScrollPos = window.pageYOffset
+      const appBar = this.$refs.appBar;
+
+      appBar.style.top = this.scrollPos > currentScrollPos ? '0' : '-100%'
+
+      this.scrollPos = currentScrollPos
+    }
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+* {
+  cursor: default;
+
+  #app-bar {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    position: fixed;
+    transition: top 0.5s;
+    height: 8em;
+    padding: 2em;
+
+    .logo {
+      height: 100%;
+
+      img {
+        height: 100%;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
